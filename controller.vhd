9780fca5daf4dacd5591930e3d7f07fda43dd65e
Vinayak Sankar <65824687+vnksnkr@users.noreply.github.com>
2021-03-26 19:02:24 +0530

    Update controller.vhd

--- controller.vhd
+++ controller.vhd
@@ -4,17 +4,17 @@ use ieee.numeric_std.all;
 
 entity controller is 
 	port(
-    	clk   	  	 : in std_logic;
+    	clk   	  	 : in std_logic;                     
         reset 	  	 : in std_logic;
         start 	  	 : in std_logic;
-        from_uart 	 : in std_logic_vector (7 downto 0);
-        from_spi  	 : in std_logic_vector (7 downto 0);
+        from_uart 	 : in std_logic_vector (7 downto 0);     --data from uart--
+        from_spi  	 : in std_logic_vector (7 downto 0);     --data from spi slave---    
         spi_recieved : in std_logic;
         
-        length	  : out std_logic_vector (4 downto 0);
-        addr	  : out std_logic_vector (1 downto 0);
-        to_spi	  : out std_logic_vector (7 downto 0);
-        to_uart   : out std_logic_vector (7 downto 0);
+        length	  : out std_logic_vector (4 downto 0);       --- length of data (8 bit or 16 bit)
+        addr	  : out std_logic_vector (1 downto 0);       --- slave address (0,1,2,3)  
+        to_spi	  : out std_logic_vector (7 downto 0);       --- data to be sent to slave---
+        to_uart   : out std_logic_vector (7 downto 0);      --- data to be sent to uart---
         
         send_to_spi  : out std_logic;
         send_to_uart : out std_logic
@@ -28,17 +28,17 @@ architecture RTL of controller is
     signal r_to_spi : std_logic_vector (7 downto 0);
     
 
-    signal mode			  : std_logic;
-    signal r_send_to_spi  : std_logic := '0';
+    signal mode			  : std_logic;         -------- read or write mode ----
+    signal r_send_to_spi  : std_logic := '0';   
     signal r_send_to_uart : std_logic := '0';
     
-    signal startcnt : integer range 0 to 2 := 0;
-    signal  bitcnt  : integer := 0;
+    signal startcnt : integer range 0 to 2 := 0; ------counter to send start signal to uart port of the bridge for sending data from spi to uart --- 
+    signal  bitcnt  : integer := 0;  
     signal length_i : integer ;    
 
     type SM is (
     CONFIGURE,WAIT_DATA,DATA,
-    RECIEVE_FROM_SPI,STOP);
+    RECIEVE_FROM_SPI,STOP);                 -----FSM  states---
     
     signal	 state :  SM := CONFIGURE;
     
@@ -51,7 +51,7 @@ architecture RTL of controller is
         	begin
             	if rising_edge(clk) then
                 	case state is
-                        when CONFIGURE =>
+                        when CONFIGURE =>                                           --- configure bridge ----
                         	if start = '0' or reset = '1' then
                             	state <= CONFIGURE;
                             else
@@ -70,7 +70,7 @@ architecture RTL of controller is
                                 end if;
                             end if;
                         
-                        when WAIT_DATA =>
+                        when WAIT_DATA =>                                       --- wait for data to be sent from uart ----
                         	if start = '0' then
                             	state <= WAIT_DATA;
                                 r_send_to_spi <= '0';
@@ -84,7 +84,7 @@ architecture RTL of controller is
                             
                             end if;
                         
-                        when DATA =>
+                        when DATA =>                                    ----send data to spi master ----
                         	r_to_spi <= from_uart;
                             r_send_to_spi <= '1';
                             if bitcnt < length_i then
@@ -97,7 +97,7 @@ architecture RTL of controller is
 
                             end if;
                             
-                        when RECIEVE_FROM_SPI =>
+                        when RECIEVE_FROM_SPI =>                    ---- recieve data from spi master ----
                         	r_send_to_spi <= '0';
                             if spi_recieved = '1' then
                             	to_uart <= from_spi;
@@ -119,7 +119,7 @@ architecture RTL of controller is
         	begin
             	
                 if rising_edge(clk) then
-                	if spi_recieved = '1' and mode = '1' then
+                	if spi_recieved = '1' and mode = '1' then          ----- sending start signal to uart port of bridge -----
                     	r_send_to_uart <= '1';
                     else
                     	if startcnt >= 2 then
@@ -135,10 +135,10 @@ architecture RTL of controller is
                 
         end process;
         
-        send_to_uart <= r_send_to_uart;
+        send_to_uart <= r_send_to_uart; 
         to_spi <= r_to_spi;
         addr <= r_addr;
         length <= r_length;
         send_to_spi <= r_send_to_spi;
 
-end RTL;
+end RTL; 

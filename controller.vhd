bc3fdcf94842892bc989318f90a1bec3841914cd
Vinayak Sankar <65824687+vnksnkr@users.noreply.github.com>
2021-03-26 20:08:31 +0530

    Update controller.vhd

--- controller.vhd
+++ controller.vhd
@@ -49,89 +49,89 @@ architecture RTL of controller is
     	length_i <= 1 when r_length(4) = '1' else 0 ;
     	process(clk)
         	begin
-            	if rising_edge(clk) then
-                	case state is
-                        when CONFIGURE =>                                           --- configure bridge ----
-                        	if start = '0' or reset = '1' then
-                            	state <= CONFIGURE;
-                            else
-                            	r_length <= from_uart(4 downto 0);
-                                r_addr   <= from_uart(6 downto 5);
-                                mode	 <= from_uart(7);
+            		if rising_edge(clk) then
+                		case state is
+                        		when CONFIGURE =>                                           --- configure bridge ----
+                        			if start = '0' or reset = '1' then
+                            				state <= CONFIGURE;
+                            			else
+                            				r_length <= from_uart(4 downto 0);
+                                			r_addr   <= from_uart(6 downto 5);
+                                			mode	 <= from_uart(7);
                                 
-                                if from_uart(7) = '0' then
-                                	state <= WAIT_DATA;
+                                			if from_uart(7) = '0' then
+                                				state <= WAIT_DATA;
                                 
-                                else
-                                	r_to_spi <= (others => 'Z');
-                                    state    <= RECIEVE_FROM_SPI;
-                                    r_send_to_spi <= '1';
+                                			else
+                                				r_to_spi <= (others => 'Z');
+                                    				state    <= RECIEVE_FROM_SPI;
+                                    				r_send_to_spi <= '1';
                                 
-                                end if;
-                            end if;
+                                			end if;
+                            			end if;
                         
-                        when WAIT_DATA =>                                       --- wait for data to be sent from uart ----
-                        	if start = '0' then
-                            	state <= WAIT_DATA;
-                                r_send_to_spi <= '0';
-                        	else
-                            	if mode = '0' then
-                                	state <= DATA;
-                                else
-                                	r_to_spi <= (others => 'Z');
-                                    state 	 <= RECIEVE_FROM_SPI;
-                                end if;
+                        		when WAIT_DATA =>                                       --- wait for data to be sent from uart ----
+                        			if start = '0' then
+                            				state <= WAIT_DATA;
+                                			r_send_to_spi <= '0';
+                        			else
+                            				if mode = '0' then
+                                				state <= DATA;
+                                			else
+                                				r_to_spi <= (others => 'Z');
+                                    				state 	 <= RECIEVE_FROM_SPI;
+                                			end if;
                             
-                            end if;
+                            			end if;
                         
-                        when DATA =>                                    ----send data to spi master ----
-                        	r_to_spi <= from_uart;
-                            r_send_to_spi <= '1';
-                            if bitcnt < length_i then
-                                bitcnt <= bitcnt + 1;
-                                state  <= WAIT_DATA;
-                            else
-                                bitcnt <= 0;
-                            	state <= STOP;
+                        		when DATA =>                                    ----send data to spi master ----
+                        			r_to_spi <= from_uart;
+                            			r_send_to_spi <= '1';
+                            			if bitcnt < length_i then
+                                			bitcnt <= bitcnt + 1;
+                                			state  <= WAIT_DATA;
+                            			else
+                                			bitcnt <= 0;
+                            				state <= STOP;
                             	
 
-                            end if;
+                           		        end if;
                             
-                        when RECIEVE_FROM_SPI =>                    ---- recieve data from spi master ----
-                        	r_send_to_spi <= '0';
-                            if spi_recieved = '1' then
-                            	to_uart <= from_spi;
-                                state <= STOP;
-                            else
-                            	state <= RECIEVE_FROM_SPI;
-                            end if;
+                        		when RECIEVE_FROM_SPI =>                    ---- recieve data from spi master ----
+                        			r_send_to_spi <= '0';
+                            			if spi_recieved = '1' then
+                            				to_uart <= from_spi;
+                                			state <= STOP;
+                            			else
+                            				state <= RECIEVE_FROM_SPI;
+                            			end if;
                         
-                        when STOP =>
-                            r_send_to_spi <= '0';
-                            state <= CONFIGURE;
+                        		when STOP =>
+                            			r_send_to_spi <= '0';
+                            			state <= CONFIGURE;
                             
-                    end case;        
-                end if;            	
+                    			end case;        
+                	end if;            	
         
         end process;
         
         process(clk)
         	begin
             	
-                if rising_edge(clk) then
-                	if spi_recieved = '1' and mode = '1' then          ----- sending start signal to uart port of bridge -----
-                    	r_send_to_uart <= '1';
-                    else
-                    	if startcnt >= 2 then
-                        	r_send_to_uart <= '0';
-                            startcnt <= 0;
+                	if rising_edge(clk) then
+                		if spi_recieved = '1' and mode = '1' then          ----- sending start signal to uart port of bridge -----
+                    			r_send_to_uart <= '1';
+                    		else
+                    			if startcnt >= 2 then
+                        			r_send_to_uart <= '0';
+                            			startcnt <= 0;
                             
-                        else
-                        	r_send_to_uart <= r_send_to_uart;
-                            startcnt <= startcnt + 1;
-                        end if;
-                    end if;
-                end if;
+                        		else
+                        			r_send_to_uart <= r_send_to_uart;
+                            			startcnt <= startcnt + 1;
+                        		end if;
+                    		end if;
+                	end if;
                 
         end process;
         

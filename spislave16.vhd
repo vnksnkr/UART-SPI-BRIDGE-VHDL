be57d64e55b657158c6dc2c78d44b76a863de05d
Vinayak Sankar <vnksnkr@gmail.com>
2021-03-28 23:53:35 +0530

    Update spislave16.vhd

--- spislave16.vhd
+++ spislave16.vhd
@@ -1,3 +1,16 @@
+----------------------------------------------------------------------------
+--  spislave16.vhd
+--	SPI SLAVE (16 bit)
+--	
+--
+--  Copyright (C) 2021 Vinayak Sankar
+--
+--	This program is free software: you can redistribute it and/or
+--	modify it under the terms of the GNU General Public License
+--	as published by the Free Software Foundation, either version
+--	2 of the License, or (at your option) any later version.
+--
+----------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;

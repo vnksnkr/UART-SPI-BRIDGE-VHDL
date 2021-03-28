a352060261cf2a4137f2a4712eaf6529ef2ac56f
Vinayak Sankar <vnksnkr@gmail.com>
2021-03-28 23:59:49 +0530

    Update uart_tx.vhd

--- uart_tx.vhd
+++ uart_tx.vhd
@@ -1,4 +1,16 @@
-
+----------------------------------------------------------------------------
+--  uart_tx.vhd
+--	UART TRANSMITTER
+--	
+--
+--  Copyright (C) 2021 Vinayak Sankar
+--
+--	This program is free software: you can redistribute it and/or
+--	modify it under the terms of the GNU General Public License
+--	as published by the Free Software Foundation, either version
+--	2 of the License, or (at your option) any later version.
+--
+----------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;

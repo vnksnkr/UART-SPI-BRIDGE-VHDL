cbd4485ff96c6814d3578f1340884f8a2fdc355f
Vinayak Sankar <vnksnkr@gmail.com>
2021-03-28 23:16:45 +0530

    Update uart_rx.vhd

--- uart_rx.vhd
+++ uart_rx.vhd
@@ -5,14 +5,14 @@ use ieee.numeric_std.all;
 entity uart_rx is
     generic (
         CLKS_PER_BIT: integer := 434
-    	    );
-    port(
+    );
+    port (
         clk : in std_logic;
         rx  : in std_logic;
         
         rx_bytes : out std_logic_vector (7 downto 0);
         rx_dv    : out std_logic
-        );
+    );
 end entity uart_rx;
 
 architecture RTL of uart_rx is
@@ -29,92 +29,71 @@ architecture RTL of uart_rx is
     
 begin
  	
-    process(clk)
-    
+    process (clk)
     begin
-    	
         if rising_edge(clk) then
         
             case state is
-            
-                when IDLE =>
-                	
+                when IDLE =>             	
                     bitcnt  <=  0;
                     clkcnt  <=  0;
-                    rx_dv_r <= '0';
-                    
+                    rx_dv_r <= '0'; 
                     if rx = '0' then
                         state <= START;
                         
                     else
                         state <= IDLE;
-	
                     end if;
                     
-                when START =>
-                	
-                    if clkcnt < (CLKS_PER_BIT-1)/2  then
-                    	
-                        if rx = '0' then
-                        	
+                when START => 	
+                    if clkcnt < (CLKS_PER_BIT-1)/2  then                   	
+                        if rx = '0' then                       	
                             clkcnt <= 0;
                             state  <= DATA;
                             
                         else
-                            state <= IDLE;
-			    
+                            state <= IDLE;			    
                         end if;
                     
                     else
                         clkcnt <= clkcnt + 1;
-                        state  <= START;
-                    
+                        state  <= START;                 
                     end if;
                 
-                when DATA =>
-                    
-                    if clkcnt < (CLKS_PER_BIT-1) then
-                    	
+                when DATA =>                   
+                    if clkcnt < (CLKS_PER_BIT-1) then                   	
                         clkcnt <= clkcnt + 1;
                         state  <= DATA;
                     
                     else
                         clkcnt <= 0;
-                        bytes(bitcnt) <= rx;
-                        
+                        bytes(bitcnt) <= rx; 
                         if bitcnt < 7 then
                             bitcnt <= bitcnt + 1;
                             state  <= DATA;
+
                         else
                             bitcnt <= 0;
                             state  <= STOP;
-
-                        end if;
-                    
+                        end if;                   
                     end if;
                     
-                when STOP =>
-                	
-                    if clkcnt < (CLKS_PER_BIT-1) then
-                    	
+                when STOP =>                	
+                    if clkcnt < (CLKS_PER_BIT-1) then                   	
                         clkcnt <= clkcnt + 1;
                         state  <= STOP;
                         
                     else
                         rx_dv_r <= '1';
                         clkcnt  <=  0;
-                        state   <= CLEANUP;
-                    
+                        state   <= CLEANUP;                   
                     end if;
                 
-                when CLEANUP =>
-                	
+                when CLEANUP =>                	
                     rx_dv_r <= '0';
                     state   <= IDLE;
-                    
-            
-            end case;
-		    
+                                
+            end case;		    
         end if;
         
     end process;
